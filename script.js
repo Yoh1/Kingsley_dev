17eb0e12512e4eabfff51ad9786ee77c3f41f2bb
Yoh1 <87152153+Yoh1@users.noreply.github.com>
2022-01-28 14:44:44 +0100

    page presque terminé

--- script.js
+++ script.js
@@ -36,7 +36,25 @@ const navSlide = () => {
 let controller = new ScrollMagic.Controller();
 let timeline =  new TimelineMax();
 
-timeline.to(".text",5,{x:500}).to('.content-images',2,{opacity:0},'-=2');
+
+
+timeline
+// .to('.rock',3,{y:-300})
+// .to('.girl',3,{y:-200},"-=3")
+.fromTo('.bg1',{y:-50},{y:0,duration:3},"-=3")
+.to('.content',3,{top:"0%"},"-=3")
+.fromTo('.content-images',{opacity:0},{opacity:1, duration:3});
+
+
+let scene = new ScrollMagic.Scene({
+    triggerElement: "section",
+    duration: "300%",
+    triggerHook: 0,
+})
+
+.setTween(timeline)
+.setPin('section')
+.addTo(controller)
 
 navSlide();
 
