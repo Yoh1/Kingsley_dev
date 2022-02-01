a2184ea38c711b06871746f430610a59afd8e893
Yoh1 <87152153+Yoh1@users.noreply.github.com>
2022-02-01 17:38:59 +0100

    portfolio terminé

--- script.js
+++ script.js
@@ -39,8 +39,6 @@ let timeline =  new TimelineMax();
 
 
 timeline
-// .to('.rock',3,{y:-300})
-// .to('.girl',3,{y:-200},"-=3")
 .fromTo('.bg1',{y:-50},{y:0,duration:3},"-=3")
 .to('.content',3,{top:"0%"},"-=3")
 .fromTo('.content-images',{opacity:0},{opacity:1, duration:3});
